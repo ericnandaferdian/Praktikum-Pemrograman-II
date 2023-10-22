6a123361bc73d020043ffb91166ff05c729991c6
ericnandaferdian <115437986+ericnandaferdian@users.noreply.github.com>
2023-10-22 20:19:56 +0800

    Delete Pemrograman II/src/MODUL1 directory

--- Pemrograman II/src/MODUL1/PRAK001_2210817210010_EricNandaFerdian.java
+++
@@ -1,7 +0,0 @@
-package MODUL1;
-public class PRAK001_2210817210010_EricNandaFerdian {
-    
-    public static void main(String[] args) throws Exception {
-        System.out.println("Selamat Pagi, Eric Nanda Ferdian \nSelamat Siang, Eric Nanda Ferdian \nSelamat Malam, Eric Nanda Ferdian");
-    }
-}
