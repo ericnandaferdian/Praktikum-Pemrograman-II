dbac860500a8bb23c8ddb5c52a4f6649f70a3366
ericnandaferdian <115437986+ericnandaferdian@users.noreply.github.com>
2023-11-21 22:30:00 +0800

    Add files via upload

---
+++ Pemrograman II/src/MODUL4/Soal2/HewanPeliharaan.java
@@ -0,0 +1,19 @@
+package MODUL4.Soal2;
+
+public class HewanPeliharaan {
+    private String nama;
+private String ras;
+public HewanPeliharaan(String n, String r) {
+nama = n;
+ras = r;
+}
+public void display() {
+System.out.println("Detail Hewan Peliharaan:");
+System.out.println("Nama hewan peliharaanku adalah : " + nama);
+System.out.println("Dengan ras : " + ras);
+}
+public void displayDetailKucing() {
+}
+public void displayDetailAnjing() {
+}
+}
---
+++ Pemrograman II/src/MODUL4/Soal2/Kucing.java
@@ -0,0 +1,14 @@
+package MODUL4.Soal2;
+
+public class Kucing extends HewanPeliharaan{
+    private String warnaBulu;
+public Kucing(String n, String r, String w) {
+super(n, r);
+warnaBulu = w;
+}
+@Override
+public void displayDetailKucing() {
+super.display();
+System.out.println("Memiliki warna bulu : " + warnaBulu);
+}
+}
